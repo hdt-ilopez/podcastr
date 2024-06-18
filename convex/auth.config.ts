9f1e8c47b84a0021f9d9b51668667e6d12b0fcb3
hdt-ilopez <150853590+hdt-ilopez@users.noreply.github.com>
2024-06-18 12:48:32 -0500

    First Commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: 'https://amusing-cattle-34.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
+
+export default authConfig;
